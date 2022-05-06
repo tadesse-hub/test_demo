4f46348ff4730e2ceb31c9cc3f2cc6c64b4dd9a8
Tadesse Kassa <tktadesse258@gmail.com>
2022-05-06 17:18:34 +0300

    Project1

--- src/main/java/net/javaguides/springboot/SpringBootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringBootBackendApplication.java
@@ -1,13 +1,20 @@
 package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@ComponentScan
+@EnableScheduling
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApplication.class, args);
 	}
-
 }
--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package net.javaguides.springboot.repository;
 
-import net.javaguides.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+import net.javaguides.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
 }
